0b6b6347b5b1800844f202e231abd8b60bcf50cd
ivodsgs87 <ivodsgs@gmail.com>
2025-12-21 19:23:44 +0000

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,16 @@
 
+const functions = require('firebase-functions');
+
+// Importar funções do Salt Edge (se existirem)
+// const saltEdge = require('./saltEdge');
+
+// Importar função de processamento de faturas
+const { processInvoice } = require('./processInvoice');
+
+// Exportar todas as funções
+exports.processInvoice = processInvoice;
+
+// Se tiveres as funções do Salt Edge, descomenta:
+// exports.createCustomer = saltEdge.createCustomer;
+// exports.listBanks = saltEdge.listBanks;
+// etc...
